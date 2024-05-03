b963d39d798d28a3f00c07b90fe6f2a32e6a6a57
mukeshpalsingh14 <mukeshpalsingh14@gmail.com>
2024-05-04 01:30:07 +0530

    added dashboard controller

---
+++ AngularAuthApi/AngularAuthApi/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using AngularAuthApi.context;
+using AngularAuthApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthApi.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        public DashboardController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var users = await _appDbContext.Users.ToListAsync();
+            //var UsersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(users);
+
+        }
+
+        [Authorize]
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<User>> GetUsers(int Id)
+        {
+            var user = await _appDbContext.Users.FindAsync(Id);
+            return user;
+        }
+    }
+}
---
+++ AngularAuthApi/AngularAuthApi/Controllers/ErrorHandlingController.cs
@@ -0,0 +1,48 @@
+﻿using AngularAuthApi.context;
+using AngularAuthApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AngularAuthApi.Controllers
+{
+    public class ErrorHandlingController : Controller
+    {
+        private readonly AppDbContext _dataContext;
+        public ErrorHandlingController(AppDbContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> getSecret()
+        {
+            return "secret test";
+        }
+        [HttpGet("not-found")]
+        public ActionResult<User> getNotFound()
+        {
+            var thing = _dataContext.Users.Find(-1);
+            if (thing == null) return NotFound();
+            return thing;
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> getServerError()
+        {
+            var thing = _dataContext.Users.Find(-1);
+            var thingToRetun = thing.ToString();
+            return thingToRetun;
+
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<string> getBadRequest()
+        {
+            return BadRequest("This was not a good request");
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
